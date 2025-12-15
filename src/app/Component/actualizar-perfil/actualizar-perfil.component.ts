1f4896ff580be48262e96cc8592a6849280f6008
ElianKun <Elian.E.K@hotmail.com>
2025-12-15 14:01:35 -0300

    ft(Component): -Add nuevo componente actualizaPerfil -Fix html de perfil

---
+++ src/app/Component/actualizar-perfil/actualizar-perfil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualizar-perfil',
+  imports: [],
+  templateUrl: './actualizar-perfil.component.html',
+  styleUrl: './actualizar-perfil.component.css'
+})
+export class ActualizarPerfilComponent {
+
+}
