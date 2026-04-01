04cc80345d0dc986b24738cc22ae24cb67107a68
Elian <Elian.E.K@hotmail.com>
2026-04-01 17:45:13 -0300

    ft(Source): -Create new folder Guard -Creat new file Admin.guard

---
+++ src/app/Guards/Admin.guard.ts
@@ -0,0 +1,34 @@
+// admin.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Observable, map, take } from 'rxjs';
+import { LoginService } from '../Servicios/auth2/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private authService: LoginService, private router: Router) {}
+
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.userLoginOn.pipe(
+      take(1),
+      map(isAuthenticated => {
+        if (!isAuthenticated) {
+          this.authService.setRedirectUrl(window.location.pathname);
+          return this.router.parseUrl('/login');
+        }
+
+        // Verificar rol de ADMIN
+        const role = this.authService.getRole();
+        if (role !== 'ADMIN') {
+          console.warn('Acceso denegado: se requiere rol ADMIN');
+          return this.router.parseUrl('/unauthorized'); // Redirigir a página de no autorizado
+        }
+
+        return true;
+      })
+    );
+  }
+}
