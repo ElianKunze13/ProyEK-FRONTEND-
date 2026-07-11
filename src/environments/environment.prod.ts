c51d14339e1ed22313050eaa1e3fef72659f7f8d
Elian <Elian.E.K@hotmail.com>
2026-07-11 17:25:04 -0300

    ft(Src): -Chore modificar aspectos para produccion -Delete SSR/Add route to environment in Angular.json -Chore modificar Package.json -Chore comentar en environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //apiUrl: 'http://www.DIRECCION_DE_EJEMPLO.com/api/v1',
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'http://localhost:8080/api/v1',//cambiar url cuando se despliege en Railway
+  //apiUrl: 'https://tu-backend-en-railway.up.railway.app/api/v1' <-- URL de ejemplo
   userId:''
 
 };
