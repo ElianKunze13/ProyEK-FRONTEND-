c3ac012ac85771380cd506f3c3113242ff7f794c
Elian <Elian.E.K@hotmail.com>
2026-07-13 11:59:10 -0300

    ft(Environment): -Add url de backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api/v1',//cambiar url cuando se despliege en Railway
+  apiUrl: 'https://proyek-backend.onrender.com/api/v1',//url de backend desplegado en Render
   //apiUrl: 'https://tu-backend-en-railway.up.railway.app/api/v1' <-- URL de ejemplo
   userId:''
 
