26462d9d6cc4512c27ec365d7b4a2f589e6f5c25
Elian <Elian.E.K@hotmail.com>
2026-07-18 15:46:43 -0300

    ft(Environment): -Chore modificar url v0.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proyek-backend.onrender.com/api/v1',//url de backend desplegado en Render
+  apiUrl: 'https://proyek-backend.onrender.com:10000/api/v1',//url de backend desplegado en Render
   userId:''
 
 };
