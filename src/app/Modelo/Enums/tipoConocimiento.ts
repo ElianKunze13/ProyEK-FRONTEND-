c5ae639535dc8fe2777a6cef2719824a489d3df2
Elian <Elian.E.K@hotmail.com>
2026-08-31 11:32:49 -0300

    ft(Enum): -Add nuevo enum DISENIO

--- src/app/Modelo/Enums/tipoConocimiento.ts
+++ src/app/Modelo/Enums/tipoConocimiento.ts
@@ -1,10 +1,10 @@
 export enum TipoConocimiento{
-    FRONTEND,
-    BACKEND,
-    BASE_DATOS,
-    TESTING,
-    IA,
-    DISENIO,
-    PROTOTIPO,
-    OTROS
+ FRONTEND = 'FRONTEND',
+    BACKEND = 'BACKEND',
+    BASE_DATOS = 'BASE_DATOS',
+    TESTING = 'TESTING',
+    IA = 'IA',
+    PROTOTIPO = 'PROTOTIPO',
+    DISENIO = 'DISENIO',  // Cambiado de 'DISEÑO' a 'DISENIO' para evitar problemas de codificación
+    OTROS = 'OTROS'
 }
